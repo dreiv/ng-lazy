d81543cec502ad196598831f0c9e4714a41b0a6e
Andrei Voicu <Andrei-Florian.Voicu@haufe-lexware.com>
2016-10-04 21:25:20 +0300

    tinkering with the webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var path = require('path');
-var webpack = require('webpack');
-var merge = require('webpack-merge');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ContextReplacementPlugin = require('webpack/lib/ContextReplacementPlugin');
+const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
 
 // Common configuration between bundles
 var commonConfig = {
@@ -21,22 +21,19 @@ var commonConfig = {
     module: {
         loaders: [
             {test: /\.ts$/, loaders: ['ts-loader', 'angular2-template-loader', 'angular2-router-loader']},
-            {test: /\.html$/, loader: 'html'},
-            {test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/, loader: 'file?name=assets/[name].[hash].[ext]'},
-            {
-                test: /\.css$/,
-                exclude: root('src', 'app'),
-                loader: ExtractTextPlugin.extract({fallbackLoader: 'style-loader', loader: 'css-loader?sourceMap'})
-            },
-            {test: /\.css$/, include: root('src', 'app'), loader: 'raw'}
+            {test: /\.html$/, loader: 'html-loader'},
+            {test: /\.css$/, include: root('src', 'app'), loader: 'raw-loader'}
         ]
     },
 
     plugins: [
-        new webpack.optimize.CommonsChunkPlugin({
+        new ContextReplacementPlugin(
+            /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+            root('./src')
+        ),
+        new CommonsChunkPlugin({
             name: ['app', 'vendor', 'polyfills']
         }),
-
         new HtmlWebpackPlugin({
             template: 'src/index.html'
         })
@@ -53,10 +50,6 @@ var devConfig = merge(commonConfig, {
         filename: '[name].js',
         chunkFilename: '[id].chunk.js'
     },
-
-    plugins: [
-        new ExtractTextPlugin('[name].css')
-    ],
 
     devServer: {
         historyApiFallback: true,
