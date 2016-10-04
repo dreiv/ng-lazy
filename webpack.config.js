e7991d43466ddd8ba6af873ab3557e4c17325c31
Andrei Voicu <Andrei-Florian.Voicu@haufe-lexware.com>
2016-10-04 13:50:57 +0300

    finally got it working :O

--- webpack.config.js
+++ webpack.config.js
@@ -24,24 +24,14 @@ var commonConfig = {
                 test: /\.ts$/,
                 loaders: ['awesome-typescript-loader', 'angular2-template-loader', 'angular2-router-loader']
             },
-            {
-                test: /\.html$/,
-                loader: 'html'
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-                loader: 'file?name=assets/[name].[hash].[ext]'
-            },
+            {test: /\.html$/, loader: 'html'},
+            {test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/, loader: 'file?name=assets/[name].[hash].[ext]'},
             {
                 test: /\.css$/,
                 exclude: root('src', 'app'),
                 loader: ExtractTextPlugin.extract({fallbackLoader: 'style-loader', loader: 'css-loader?sourceMap'})
             },
-            {
-                test: /\.css$/,
-                include: root('src', 'app'),
-                loader: 'raw'
-            }
+            {test: /\.css$/, include: root('src', 'app'), loader: 'raw'}
         ]
     },
 
